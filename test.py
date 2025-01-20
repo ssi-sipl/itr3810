14cb5d0a66450697edca796ffc736a21b3e7d4a7
abhinnvyas <abhinnvyas2@gmail.com>
2025-01-20 12:07:02 +0530

    fixesss2

--- test.py
+++ test.py
@@ -84,7 +84,7 @@ print("Description Result: ", result)
 
 if result == 0:
     print(f"Description Length: {description.descriptionLength}")
-    print(f"Description: {description.description.decode('utf-8', 'ignore')}")
+    print(f"Description: {description.description}")
 else:
     print(f"Error occurred: {result}")
 
