18a427c8351c03535b1a897a30616e8d245c317e
abhinnvyas <abhinnvyas2@gmail.com>
2025-02-04 17:49:20 +0530

    changes

--- main.py
+++ main.py
@@ -331,7 +331,9 @@ def parse_object_list(object_list):
 def main():
     # radar = RadarAPI("./Software/RadarAPI/library_v1.147/Windows_msvc_2017_x64/ITR3800_radarAPI.dll")
 
-    radar = RadarAPI("./Software/RadarAPI/library_v1.147/Linux_x64/libITR3800_radarAPI.so")
+    # radar = RadarAPI("./Software/RadarAPI/library_v1.147/Linux_x64/libITR3800_radarAPI.so")
+    radar = RadarAPI("./Software/RadarAPI/library_v1.147/Linux_aarch64/libITR3800_radarAPI.so")
+
     try:
         print(f"Radar API Version: {radar.get_api_version()}")
         radar.init_system(RADAR_IP)
